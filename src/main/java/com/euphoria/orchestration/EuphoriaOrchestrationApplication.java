88898aa1f6129fe53fcd89ffc879329bd5187948
nandakumark <nandakumark6693@gmail.com>
2020-05-08 22:15:33 +0530

    Add project base template

---
+++ src/main/java/com/euphoria/orchestration/EuphoriaOrchestrationApplication.java
@@ -0,0 +1,13 @@
+package com.euphoria.orchestration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuphoriaOrchestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuphoriaOrchestrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/euphoria/euphoriaorchestration/EuphoriaOrchestrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euphoria.euphoriaorchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuphoriaOrchestrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
