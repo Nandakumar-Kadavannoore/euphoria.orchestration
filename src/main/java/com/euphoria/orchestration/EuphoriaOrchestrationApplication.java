907bafb0cc7771218040937c275c030e2e803b00
nandakumark <nandakumark>
2020-05-27 22:43:47 +0530

    Add Eureka client, OpenAPI

--- src/main/java/com/euphoria/orchestration/EuphoriaOrchestrationApplication.java
+++ src/main/java/com/euphoria/orchestration/EuphoriaOrchestrationApplication.java
@@ -2,8 +2,10 @@ package com.euphoria.orchestration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EuphoriaOrchestrationApplication {
 
 	public static void main(String[] args) {
